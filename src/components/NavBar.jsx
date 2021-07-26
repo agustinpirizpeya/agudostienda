fd7d91776c7e587b98bd4bca2d753790d5db5b87
agustinpirizpeya <57919983+agustinpirizpeya@users.noreply.github.com>
2021-07-26 17:29:08 -0300

    Add navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import './navBar.css';
+import React from 'react';
+import logo from '../assets/icons/logo-market.svg'
+
+function NavBar(){
+    return(
+        <div className='navbar'>
+            <div className='logo'>
+            <div> <img src={logo}/> </div>
+            </div>
+                <div className="topnav">
+                    <a href="#home" className="active">Home</a>
+                    <a href="#news">News</a>
+                    <a href="#contact">Contact</a>
+                </div>
+        </div>
+    );
+};
+
+export default NavBar;
