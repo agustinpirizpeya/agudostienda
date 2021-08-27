2faac0d140a9708dea9a65bc432db1c7941d4bfb
agustinpirizpeya <57919983+agustinpirizpeya@users.noreply.github.com>
2021-08-26 22:08:46 -0300

    Add item in cart with context

--- src/App.js
+++ src/App.js
@@ -1,33 +1,62 @@
 import NavBar from "./modules/global/NavBar/NavBar";
 import "./app.css";
-import React from "react";
+import { useState, React } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { CartContext } from "./context/cartContext";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
+
+  const [cartItems, setCartItems ] = useState([]);
+
+  const addItem = ( item ) => {
+    console.log('Agrego Item');
+    debugger;
+    if(!isInCart){
+      setCartItems(
+        ...cartItems,
+        item);
+    }
+  }
+
+  const clear = () => {
+    console.log('limpio carrito');
+  }
+  
+  const removeItem = () => {
+    console.log('Borro item carrito');
+  }
+
+  const isInCart = ( idItem ) => {
+    return cartItems.map(item => item.id === idItem);
+    console.log('Esta en el carrito');
+  }
+
   return (
-    <BrowserRouter>
-      <div className="main">
-        <header>
-          <NavBar />
-        </header>
-      </div>
-      <Switch>
-        <Route exact path="/">
-          <ItemListContainer />
-        </Route>
-        <Route exact path="/category/:id">
-          <ItemListContainer />
-        </Route>
-        <Route exact path="/item/:id">
-          <ItemDetailContainer />
-        </Route>
-        <Route exact path="/cart">
-          <ItemListContainer/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <CartContext.Provider value={{ cartItems, addItem, clear, removeItem, isInCart }}>
+      <BrowserRouter>
+        <div className="main">
+          <header>
+            <NavBar />
+          </header>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <ItemListContainer />
+          </Route>
+          <Route exact path="/category/:id">
+            <ItemListContainer />
+          </Route>
+          <Route exact path="/item/:id">
+            <ItemDetailContainer />
+          </Route>
+          <Route exact path="/cart">
+            <ItemListContainer />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </CartContext.Provider>
   );
 }
 
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,20 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import ItemCount from "../ItemCount/ItemCount";
 import Button from "../Button/Button";
+import { CartContext } from "../../context/cartContext";
 import { Link } from "react-router-dom";
 import "./itemDetail.css";
 
 export default function ItemDetail(props) {
+  const { addItem } = useContext(CartContext);
   const [items, setItems] = useState(0);
+  const [cartItem, setCartItem] = useState([]);
 
   useEffect(() => {
     showFinishPurchase();
-    console.log(showFinishPurchase());
+    console.log("CANTIDADD", items);
+    console.log("THeme", addItem);
   }, [items]);
 
-  const showFinishPurchase = () => { return items > 0 } 
+  useEffect(() => {
+    addItem(cartItem);
+  }, [cartItem]);
 
-  useEffect(() => {}, [items]);
+  const showFinishPurchase = () => {
+    return items > 0;
+  };
 
   return (
     <div className="detailContainer">
@@ -29,7 +37,22 @@ export default function ItemDetail(props) {
         <h3>{props.itemDetail.created}</h3>
         <ItemCount stock={5} initial={0} onAdd={setItems} items={items} />
         {showFinishPurchase && (
-           <Link to="/cart"> Terminar mi compra </Link>
+          <Link to="/cart">
+            {" "}
+            <button
+              onClick={() =>
+                setCartItem([
+                  {
+                    ...cartItem,
+                    item: props.itemDetail,
+                    quantity: items,
+                  },
+                ])
+              }
+            >
+              Terminar mi compra
+            </button>{" "}
+          </Link>
         )}
       </div>
     </div>
